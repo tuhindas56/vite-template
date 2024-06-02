005e6c2f72d155c03a9dcdcf79e1fc70f3790aee
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-02 17:30:19 +0530

    chore: Add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+}
