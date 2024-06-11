f0857e43ea23a9d766fa3b34e6bdd2a152f2d8d6
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2024-06-11 13:09:25 +0530

    Update paths in script

--- sass.sh
+++ sass.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo "Removing TailwindCSS and relevant packages and files.."
 npm remove tailwindcss @tailwindcss/forms prettier-plugin-tailwindcss
-rm src/style.css src/main.ts tailwind.config.js
+rm src/style.css src/ts/main.ts tailwind.config.js
 clear
 echo "Installing Sass.."
 npm i -D sass @fullhuman/postcss-purgecss
@@ -12,7 +12,7 @@ echo "Adding basic CSS Reset and base styles.."
 echo "*, *::before, *::after {box-sizing: border-box;}* {margin: 0;}body {line-height: 1.5;-webkit-font-smoothing: antialiased;min-height: 100vh;}img, picture, video, canvas, svg {display: block;max-width: 100%;}input, button, textarea, select {font: inherit;}p, h1, h2, h3, h4, h5, h6 {overflow-wrap: break-word;}#root, #__next {isolation: isolate;}" > src/scss/_base.scss
 echo "@use 'base';" > src/scss/main.scss
 echo "Adding import for main.scss to main.ts.."
-echo "import './scss/main.scss'" > src/main.ts
+echo "import '../scss/main.scss'" > src/ts/main.ts
 echo "Updating postcss configuration.."
 rm postcss.config.js
 echo "const purgecss = require('@fullhuman/postcss-purgecss'); module.exports = ({ env }) => ({plugins: env === 'production' ? [require('autoprefixer'), purgecss({ content: ['./index.html', './src/**/*.ts'] }),] : [],})" > postcss.config.cjs
@@ -33,3 +33,4 @@ npx prettier vite.config.ts --write
 clear
 echo "Project setup successful!"
 rm sass.sh
+rm removeTailwind.sh
